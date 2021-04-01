a872081311a78fd7ccccc2cedd18aa0455bc0392
Gaurav Patel <GauravPatel97@outlook.com>
2021-04-01 23:46:11 +0530

    Added more examples

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ mod image_io;
 
 fn main() {
     let read_path = "/home/gp/Documents/duotone/assets/t5.png";
-    let save_path = "/home/gp/Documents/duotone/assets/t5_duotone.png";
+    let save_path = "/home/gp/Documents/duotone/assets/t5_duotone4.png";
 
     let mut img = image_io::read(read_path);
 
-    let light_color: [f32; 3] = [0.0, 255.0, 255.0];
-    let dark_color: [f32; 3] = [0.0, 0.0, 0.0];
+    let light_color: [f32; 3] = [0.0, 0.0, 90.0];
+    let dark_color: [f32; 3] = [203.0, 0.0, 0.0];
     let contrast: f32 = -10.0;
 
     let img_duotoned = duotone::duotone(&mut img, light_color, dark_color, contrast);
